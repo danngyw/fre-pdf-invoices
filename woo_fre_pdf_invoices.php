8f78f847277eb0dca5ed518b0a3ed2be43621312
Dan Ngyen <wpcodev@gmail.com>
2021-02-04 21:28:22 +0700

    update

--- woo_fre_pdf_invoices.php
+++ woo_fre_pdf_invoices.php
@@ -45,13 +45,13 @@ function fre_send_mail_vs_attachment_debug(){
 
 
 
-	// $oder_id 		= 239;
-	// $attachments 	= array();
-	// $attachments 	= fre_pdf_get_file($oder_id);
+	$oder_id 		= 239;
+	$attachments 	= array();
+	$attachments 	= fre_pdf_get_file($oder_id);
 
-	// $attachments = array( WP_CONTENT_DIR . '/uploads/wpo_wcpdf/invoiceinvoice-239.pdf' );
-	// $attachments = array($pdf_file[0] );
-	//$t = wp_mail('danhoat@gmail.com', 'Mail Subject For Test PDF Attach File '. time(),  'Test Content', $header = '', $attachments);
+	$attachments = array( WP_CONTENT_DIR . '/uploads/wpo_wcpdf/invoiceinvoice-239.pdf' );
+	$attachments = array($pdf_file[0] );
+	$t = wp_mail('danhoat@gmail.com', 'Mail Subject For Test PDF Attach File '. time(),  'Test Content', $header = '', $attachments);
 }
 add_action('wp_footer','fre_send_mail_vs_attachment_debug');
 
